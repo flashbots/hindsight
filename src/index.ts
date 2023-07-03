2ea8c6d601e5608c0f64ae1d46ae12993a82255c
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-07-02 20:17:20 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -47,19 +47,20 @@ async function main() {
     let eligibleEvents: EventHistoryEntry[] = cachedEvents
     let eligibleTxs: TransactionResponseParams[] = cachedTxs
 
+    const uniswapTopics = [
+        // univ3
+        // Swap(address,address,int256,int256,uint160,uint128,int24)
+        "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67",
+        // univ2
+        // Sync(uint112,uint112)
+        "0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1",
+        // univ2
+        // Swap(address,uint256,uint256,uint256,uint256,address)
+        "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822",
+    ]
+
     if (!skipDownload) {
         // find uniswap-related events
-        const uniswapTopics = [
-            // univ3
-            // Swap(address,address,int256,int256,uint160,uint128,int24)
-            "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67",
-            // univ2
-            // Sync(uint112,uint112)
-            "0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1",
-            // univ2
-            // Swap(address,uint256,uint256,uint256,uint256,address)
-            "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822",
-        ]
         eligibleEvents = await hindsight.filterEvents(events, uniswapTopics)
 
         // get raw txs of eligible events
@@ -67,7 +68,7 @@ async function main() {
     }
 
     // ... doing stuff with the transactions ...
-    console.log("eligible txs", Signature.from(eligibleTxs[0].signature).serialized)
+    console.log("testing... eligible tx sig", Signature.from(eligibleTxs[0].signature).serialized)
     // TODO: add simulations to Hindsight, then invoke that here
 
     if (!skipDownload) {
