513dd48b1934a39c5870ade2388ddffaf07adf5d
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-07-18 18:08:17 -0700

    only terminate sim on 0-range @ depth>0 if profit is negative

--- simulator/src/sim/core.rs
+++ simulator/src/sim/core.rs
@@ -240,7 +240,10 @@ async fn step_arb(
 
     if let Some(depth) = depth {
         // stop case: we have recursed once and the range minimum is still 0
-        if range[0] == 0.into() && depth > 1 {
+        if range[0] == 0.into()
+            && depth >= 1
+            && best_amount_in_out.1 < braindance_starting_balance()
+        {
             // Return (0, 0) to indicate that there was no arbitrage opportunity,
             // but the arb params (tokens, pools, etc) were still valid.
             // This ensures that the attempt is logged in the DB.
