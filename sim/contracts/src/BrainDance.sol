b3499c0a35a5fa2e2085318de37a8aca3ddc607e
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-11-18 07:10:44 -0800

    fix old import

--- sim/contracts/src/BrainDance.sol
+++ sim/contracts/src/BrainDance.sol
@@ -6,7 +6,7 @@ Credit: mouseless [rusty-sando](https://raw.githubusercontent.com/mouseless-eth/
 
 import "./interfaces/IERC20.sol";
 import "v2-core/interfaces/IUniswapV2Pair.sol";
-import "v2-periphery/interfaces/IUniswapV2Router02.sol";
+import "v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "v3-core/interfaces/IUniswapV3Pool.sol";
 import "./lib/SafeMath.sol";
 
